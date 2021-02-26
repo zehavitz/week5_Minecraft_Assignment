8906634b1b878ba851d7d3f8801ce1446b69e515
Zehavit Zanzuri <zahavitz@gmail.com>
2021-02-26 18:33:41 +0200

    add functions

--- index.js
+++ index.js
@@ -1,5 +1,56 @@
+let currentTool = "";
+let lastItemRemoved = "";
+
+const resetActiveTool = () => {
+    let previousTool = document.getElementById(currentTool);
+    if (previousTool) { previousTool.classList.remove("activeTool") }
+    currentTool = "";
+}
+
+const resetInventory = () => {
+    lastItemRemoved = "";
+    let inventory = document.getElementById("inventory");
+    inventory.className = "";
+}
+
+const onClickToolEvent = (toolName) => {
+    resetActiveTool();
+    if (toolName !== "inventory" || (toolName == "inventory" && lastItemRemoved)) {
+        currentTool = toolName;
+        let clickedTool = document.getElementById(toolName);
+        clickedTool.classList.add("activeTool");
+    }
+}
+
+
+const newCellEvent = () => {
+    let cells = document.querySelectorAll("td");
+    cells.forEach(cell => {
+        cell.addEventListener("click", () => {
+            if ((currentTool == "pickaxe" && cell.className == "rock") ||
+                (currentTool == "shovel" && (cell.className == "soil" || cell.className == "grass")) ||
+                (currentTool == "axe" && (cell.className == "treeLeaves" || cell.className == "wood"))) {
+                lastItemRemoved = cell.className;
+                let inventory = document.getElementById("inventory");
+                inventory.className = cell.className;
+                cell.className = "";
+            } 
+            else if (currentTool=="inventory") {
+                cell.className=lastItemRemoved;
+                currentTool="";
+                resetInventory();
+            }
+        });
+    }
+    )
+}
+
+
+
 //drawing the game background 
 const newGame = () => {
+    resetActiveTool();
+    resetInventory();
     let gameBackground = document.getElementById("gameBackground");
     let gameCells = "";
     for (let row = 0; row < 20; row++) {
@@ -25,17 +76,17 @@ const newGame = () => {
             }
 
             //adding tree wood
-            if ((column == 25 && (row>=8 && row <= 10))) {
+            if ((column == 25 && (row >= 8 && row <= 10))) {
                 color = "wood"
             }
 
             //adding tree leaves
-             if (( (column>=24 && column <= 26) && (row>=5 && row <= 7))) {
+            if (((column >= 24 && column <= 26) && (row >= 5 && row <= 7))) {
                 color = "treeLeaves"
             }
 
             //adding cloud
-            if (( (column>=12 && column <= 17) && (row>=4 && row <=6 ))) {
+            if (((column >= 12 && column <= 17) && (row >= 4 && row <= 6))) {
                 color = "cloud"
             }
 
@@ -45,18 +96,14 @@ const newGame = () => {
     }
 
     gameBackground.innerHTML = gameCells;
+    newCellEvent();
 }
 
 newGame();
 
 
-let cells = document.querySelectorAll("td");
-cells.forEach(cell => {
-    cell.addEventListener("click", () => {
-        cell.className = "";
-    });
-}
-)
+
+
 
 
 
